c579406833cca4a714da4c72a31a2859a862a3b2
Fernanda <fernandamora88@gmail.com>
2017-04-17 08:40:13 -0700

    add heap sort

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,6 +1,19 @@
+require 'byebug'
 require_relative "heap"
 
 class Array
   def heap_sort!
+    prc = Proc.new { |el1, el2| el2 <=> el1 }
+
+    (1..self.length).each do |i|
+      BinaryMinHeap.heapify_up(self, i - 1, i, &prc)
+    end
+
+    (self.length - 1).downto(0).each do |i|
+      self[0], self[i] = self[i], self[0]
+      BinaryMinHeap.heapify_down(self, 0, i, &prc)
+    end
+
+    self
   end
 end
